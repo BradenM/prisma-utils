58d108ff7c24d9d8ac5c4dec468d393cf55ab526
Braden Mars <bradenmars@bradenmars.me>
2022-02-09 01:32:40 -0600

    feat(models): allow either model name or model interface for util types

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,14 +1,31 @@
-import { ModelName, PrismaTypes } from './types'
+import { KeyFor, XOR } from '@prisma-model-types/shared'
+import { ModelInterfaces, ModelName, PrismaTypes } from './types'
 
+/**
+ * Take model name (as string) or model interface exclusively.
+ */
+type ModelNameOrModel = XOR<ModelName, ModelInterfaces[keyof ModelInterfaces]>
+
+/**
+ * Resolve Prisma args/action/input/output/etc type for given model.
+ */
 export type PrismaTypeFor<
-  ModelT extends ModelName,
+  ModelT,
   Name extends string,
-  _T = `${ModelT}${Name}`
+  _Model = ModelT extends ModelName
+    ? ModelT
+    : ModelT extends ModelInterfaces[keyof ModelInterfaces]
+    ? KeyFor<ModelInterfaces, ModelT>
+    : never,
+  _T = _Model extends string ? `${_Model}${Name}` : never
 > = _T extends keyof PrismaTypes ? PrismaTypes[_T] : never
 
-export type CreateFor<T extends ModelName> = PrismaTypeFor<T, 'CreateInput'>
-export type ArgsFor<T extends ModelName> = PrismaTypeFor<T, 'Args'>
-export type FindManyArgsFor<T extends ModelName> = PrismaTypeFor<
+export type CreateFor<T extends ModelNameOrModel> = PrismaTypeFor<
+  T,
+  'CreateInput'
+>
+export type ArgsFor<T extends ModelNameOrModel> = PrismaTypeFor<T, 'Args'>
+export type FindManyArgsFor<T extends ModelNameOrModel> = PrismaTypeFor<
   T,
   'FindManyArgs'
 >
