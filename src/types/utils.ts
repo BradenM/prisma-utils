256a6522b6eb401e6a8d77019f9b62e4c4b0a2a4
Braden Mars <bradenmars@bradenmars.me>
2022-02-09 01:47:12 -0600

    feat(models): extract and export NameForModel type

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -4,19 +4,27 @@ import { ModelInterfaces, ModelName, PrismaTypes } from './types'
 /**
  * Take model name (as string) or model interface exclusively.
  */
-type ModelNameOrModel = XOR<ModelName, ModelInterfaces[keyof ModelInterfaces]>
+export type ModelNameOrModel = XOR<
+  ModelName,
+  ModelInterfaces[keyof ModelInterfaces]
+>
 
 /**
- * Resolve Prisma args/action/input/output/etc type for given model.
+ * Resolve (string) name from model name or model interface.
+ */
+export type NameForModel<ModelT> = ModelT extends ModelName
+  ? ModelT
+  : ModelT extends ModelInterfaces[keyof ModelInterfaces]
+  ? KeyFor<ModelInterfaces, ModelT>
+  : never
+
+/**
+ * Resolve Prisma args/action/input/output/etc. type for given model.
  */
 export type PrismaTypeFor<
   ModelT,
   Name extends string,
-  _Model = ModelT extends ModelName
-    ? ModelT
-    : ModelT extends ModelInterfaces[keyof ModelInterfaces]
-    ? KeyFor<ModelInterfaces, ModelT>
-    : never,
+  _Model = NameForModel<ModelT>,
   _T = _Model extends string ? `${_Model}${Name}` : never
 > = _T extends keyof PrismaTypes ? PrismaTypes[_T] : never
 
