58a1056e930ab47056c92a1907df4aefe0bd680c
Braden Mars <bradenmars@bradenmars.me>
2022-03-23 06:47:02 -0500

    feat(pkg): update unbuild configs

--- build.config.ts
+++ build.config.ts
@@ -1,7 +1,14 @@
+import { fileURLToPath } from 'mlly'
 import { defineBuildConfig } from 'unbuild'
 import pkg from './package.json'
+
 export default defineBuildConfig({
   entries: ['src/index'],
   declaration: true,
+  rollup: {
+    esbuild: {
+      tsconfig: fileURLToPath(new URL('./tsconfig.build.json', import.meta.url))
+    }
+  },
   externals: [...Object.keys(pkg.dependencies), 'ts-toolbelt', 'type-fest']
 })
