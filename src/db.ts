b7098989a61f7924328b77f2dec5ba57ae50c911
Braden Mars <bradenmars@bradenmars.me>
2022-04-01 07:47:49 -0500

    feat(models): add doDbPush/execDbPush utils

---
+++ src/db.ts
@@ -0,0 +1,52 @@
+import path from 'node:path'
+import { URL, fileURLToPath } from 'node:url'
+import { DbPush } from '@prisma/migrate'
+import consola from 'consola'
+import { execa } from 'execa'
+
+export const getSchemaPath = () =>
+  path.resolve(
+    fileURLToPath(new URL('../prisma/schema.prisma', import.meta.url))
+  )
+
+export const doDbPush = async (schemaPath?: string) => {
+  const schema = schemaPath ?? getSchemaPath()
+  consola.log('Pushing database...', { schema })
+  const result = await DbPush.new().parse([
+    `--schema=${schema}`,
+    '--skip-generate'
+  ])
+  consola.log(result)
+  return result
+}
+
+export const execDbPush = async (
+  opts: {
+    schemaPath?: string
+    dbUri?: string
+  } = {}
+) => {
+  const { schemaPath = getSchemaPath(), dbUri } = opts
+  const schema = schemaPath ?? getSchemaPath()
+  const pkgDir = path.resolve(fileURLToPath(new URL('../', import.meta.url)))
+  const proc = execa(
+    'prisma',
+    ['db', 'push', `--schema=${schema}`, '--skip-generate'],
+    {
+      all: true,
+      cwd: pkgDir,
+      localDir: pkgDir,
+      preferLocal: true,
+      windowsHide: false,
+      ...(dbUri
+        ? {
+            env: {
+              DATABASE_URL: dbUri
+            }
+          }
+        : {})
+    }
+  )
+  proc.all!.pipe(process.stdout)
+  await proc
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './client'
 export * from './types/types'
 export * from './types/utils'
+export * from './db'
