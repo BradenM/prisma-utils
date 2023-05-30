05fa3b4208e1b881ad718bf25c79ffd2da127a2f
Braden Mars <bradenmars@bradenmars.me>
2023-05-30 17:38:35 -0500

    feat(projenrc): add direnv, setup shared .envrc
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,6 +1,7 @@
 import { LintConfig } from '@arroyodev-llc/projen.component.linting'
 import { ToolVersions } from '@arroyodev-llc/projen.component.tool-versions'
 import { MonorepoProject } from '@arroyodev-llc/projen.project.nx-monorepo'
+import { DirEnv } from '@arroyodev-llc/projen.component.dir-env'
 
 const monorepo = new MonorepoProject({
 	defaultReleaseBranch: 'main',
@@ -9,6 +10,7 @@ const monorepo = new MonorepoProject({
 		'@arroyodev-llc/projen.project.nx-monorepo',
 		'@arroyodev-llc/projen.project.typescript',
 		'@arroyodev-llc/projen.component.tool-versions',
+		'@arroyodev-llc/projen.component.dir-env',
 		'@arroyodev-llc/projen.component.linting',
 		'@arroyodev-llc/projen.component.pnpm-workspace',
 	],
@@ -22,9 +24,14 @@ const monorepo = new MonorepoProject({
 new LintConfig(monorepo)
 new ToolVersions(monorepo, {
 	tools: {
+		direnv: ['2.32.3'],
 		nodejs: ['18.15.0'],
 		pnpm: ['8.6.0'],
 	},
+})
+new DirEnv(monorepo).buildDefaultEnvRc({
+	localEnvRc: '.envrc.local',
+	minDirEnvVersion: '2.32.3',
 })
 
 monorepo.synth()
