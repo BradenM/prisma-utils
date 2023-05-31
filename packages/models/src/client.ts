2f8ea58c1179926e986ac2f8347297a59203dccd
Braden Mars <bradenmars@bradenmars.me>
2023-05-31 01:36:55 -0500

    feat(models): remove rest of old models package
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/models/src/client.ts
+++
@@ -1,6 +0,0 @@
-import type { PrismaClient } from '@prisma/client'
-import pkg from '@prisma/client'
-
-const PClient = pkg.PrismaClient
-export { PClient }
-export type { PrismaClient }
--- packages/models/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { PClient } from './client'
-export * from './types/types'
-export * from './types/utils'
-export * from './db'
