af79f87b387f42582182e62a17f4be96f8223358
Braden Mars <bradenmars@bradenmars.me>
2023-05-31 01:39:08 -0500

    feat(model-types): build ts via ast, inline type utilities, use ambient module
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- packages/prisma-model-types/src/generator.ts
+++ packages/prisma-model-types/src/generator.ts
@@ -1,19 +1,189 @@
-import path from 'node:path'
-import type {
-  DMMF,
-  GeneratorConfig,
-  GeneratorManifest,
-  GeneratorOptions
+import { fileURLToPath } from 'url'
+import {
+	type DMMF,
+	type GeneratorConfig,
+	generatorHandler,
+	type GeneratorManifest,
+	type GeneratorOptions,
 } from '@prisma/generator-helper'
-import { generatorHandler } from '@prisma/generator-helper'
 import sdk from '@prisma/sdk'
 import fse from 'fs-extra'
+import path from 'pathe'
+import {
+	type InterfaceDeclarationStructure,
+	type OptionalKind,
+	Project,
+	type PropertySignatureStructure,
+	type SourceFile,
+} from 'ts-morph'
 
 /**
  * Basic Prisma generator that outputs helper types.
  */
 
 const { logger } = sdk
+
+interface BuildOptions {
+	filePath: string
+	models: DMMF.Model[]
+	enums: DMMF.DatamodelEnum[]
+}
+
+const INTERFACE_NAMES = [
+	'CreateInput',
+	'UpdateInput',
+	'WhereInput',
+	'WhereUniqueInput',
+	'Select',
+	'Include',
+	'Args',
+	'CreateArgs',
+	'UpdateArgs',
+	'UpsertArgs',
+	'FindUniqueArgs',
+	'FindFirstArgs',
+	'FindManyArgs',
+	'DeleteArgs',
+]
+
+// language=typescript
+const HELPERS = `
+
+/**
+ * Take model name (as string) or model interface exclusively.
+ */
+export type ModelNameOrModel = ModelName | AnyModel
+
+export type IsAnyModelOrModelName<T> = [ModelNameOrModel] extends [T]
+	? 1
+	: [ModelName] extends [T]
+		? 1
+		: 0
+
+/**
+ * Resolve (string) name from model name or model interface.
+ */
+export type NameForModel<ModelT> = IsAnyModelOrModelName<ModelT> extends 1
+	? ModelName
+	: ModelT extends ModelName
+		? ModelT
+		: ModelT extends ModelInterfaces[keyof ModelInterfaces]
+			? KeyFor<ModelInterfaces, ModelT>
+			: never
+
+/**
+ * Resolve (interface) model from model name or model.
+ */
+export type ModelFromName<ModelOrName> =
+	IsAnyModelOrModelName<ModelOrName> extends 1
+		? AnyModel
+		: ModelOrName extends ModelName
+			? ModelInterfaces[ModelName]
+			: ModelOrName extends ModelInterfaces[keyof ModelInterfaces]
+				? ModelInterfaces[NameForModel<ModelOrName>]
+				: never
+
+/**
+ * Resolve Prisma args/action/input/output/etc. type for given model.
+ */
+export type PrismaTypeFor<
+	ModelT,
+	Name extends keyof ModelParams,
+	_Model extends string = NameForModel<ModelT>
+> = IsAnyModelOrModelName<ModelT> extends 1
+	? ModelParams[Name]
+	: _Model extends keyof ParamsByModel
+		? ParamsByModel[_Model][Name]
+		: ModelParams[Name]
+
+export type ModelParamsFor<
+	ModelT,
+	_Model = NameForModel<ModelT>
+> = IsAnyModelOrModelName<ModelT> extends 1
+	? ModelTypes.ModelParams
+	: _Model extends keyof ParamsByModel
+		? ParamsByModel[_Model]
+		: ModelTypes.ModelParams
+
+export type CreateFor<T extends ModelNameOrModel> = PrismaTypeFor<
+	T,
+	'CreateInput'
+>
+
+export type UpdateFor<T extends ModelNameOrModel> = PrismaTypeFor<
+	T,
+	'UpdateInput'
+>
+
+export type ArgsFor<T extends ModelNameOrModel> = PrismaTypeFor<T, 'Args'>
+export type CreateArgsFor<T extends ModelNameOrModel> = PrismaTypeFor<
+	T,
+	'CreateArgs'
+>
+export type UpdateArgsFor<T extends ModelNameOrModel> = PrismaTypeFor<
+	T,
+	'UpdateArgs'
+>
+
+export type UpsertArgsFor<T extends ModelNameOrModel> = PrismaTypeFor<
+	T,
+	'UpsertArgs'
+>
+
+export type FindManyArgsFor<T extends ModelNameOrModel> = PrismaTypeFor<
+	T,
+	'FindManyArgs'
+>
+
+export type FindFirstArgsFor<T extends ModelNameOrModel> = PrismaTypeFor<
+	T,
+	'FindFirstArgs'
+>
+
+export type FindUniqueArgsFor<T extends ModelNameOrModel> = PrismaTypeFor<
+	T,
+	'FindUniqueArgs'
+>
+
+export type SelectFor<T extends ModelNameOrModel> = PrismaTypeFor<T, 'Select'>
+
+export type IncludeFor<T extends ModelNameOrModel> = PrismaTypeFor<T, 'Include'>
+
+export type WhereFor<T extends ModelNameOrModel> = PrismaTypeFor<
+	T,
+	'WhereInput'
+>
+
+export type WhereUniqueFor<T extends ModelNameOrModel> = PrismaTypeFor<
+	T,
+	'WhereUniqueInput'
+>
+
+export type DelegateFor<T extends ModelNameOrModel> = PrismaTypeFor<
+	T,
+	'Delegate'
+>
+
+export const validatorFor = <
+	ModelT extends ModelNameOrModel,
+	_ModelArgs = ArgsFor<ModelT>
+>() => pkg.Prisma.validator<_ModelArgs>()
+
+/**
+ * Resolve Prisma args/action/input/output/etc. type for given model.
+ */
+export type PayloadFor<
+	ModelT extends ModelNameOrModel,
+	T extends ArgsFor<ModelT> | undefined | null | boolean = undefined,
+	// _Model = NameForModel<ModelT>
+	_Model extends string = NameForModel<ModelT>
+	// _Model extends string = NameForModel<ModelT> extends string ? NameForModel<ModelT> : NameForModel<ModelT>
+> = IsAnyModelOrModelName<ModelT> extends 1
+	? ModelTypes.ModelPayloadParams['Payload']
+	: _Model extends keyof PayloadParamsByModel<any>
+		? PayloadParamsByModel<T>[_Model]['Payload']
+		: ModelTypes.ModelPayloadParams['Payload']
+	`
 
 /**
  * Generates types from DMMF data.
@@ -32,159 +202,227 @@ const { logger } = sdk
  * Despite this, the TS Lang Server (via webstorm at least) correctly infers the type and provides
  * autocompletion. (again, this being with the code failing to compile via tsc).
  *
- * This makes me think it may be a possible bug in TS itself...
- *
- * @param models - DMMF models.
- * @param enums - DMMF enums.
+ * @param filePath Write target path.
+ * @param models DMMF models.
+ * @param enums DMMF enums.
  */
-const createInterface = (
-  models: DMMF.Model[],
-  enums: DMMF.DatamodelEnum[]
-): string => {
-  // Workaround interface for namespace issue.
-  const modelNames = models
-    .map(({ name }) => name)
-    .sort((a, b) => b.length - a.length)
-  const interfaceNames = [
-    'CreateInput',
-    'UpdateInput',
-    'WhereInput',
-    'WhereUniqueInput',
-    'Select',
-    'Include',
-    'Args',
-    'CreateArgs',
-    'UpdateArgs',
-    'UpsertArgs',
-    'FindUniqueArgs',
-    'FindFirstArgs',
-    'FindManyArgs',
-    'DeleteArgs'
-  ]
+const build = ({ filePath, models, enums }: BuildOptions): SourceFile => {
+	const project = new Project({
+		skipAddingFilesFromTsConfig: true,
+	})
+	const source = project.createSourceFile(filePath, ``, { overwrite: true })
+	const module = source.addModule({
+		name: `'prisma-model-types/model-types'`,
+		hasDeclareKeyword: true,
+	})
 
-  // Per-model parameters interface.
-  const modelParams = modelNames.map(
-    (mname) => `export interface ${mname}Params {
-    model: "${mname}"
-${interfaceNames
-  .map((iname) => `  ${iname}: Prisma.${mname}${iname}`)
-  .join('\n')}
-    Delegate: Prisma.${mname}Delegate<Prisma.RejectOnNotFound | Prisma.RejectPerOperation>
-}`
-  )
+	// Workaround interface for namespace issue.
+	const modelNames = models
+		.map(({ name }) => name)
+		.sort((a, b) => b.length - a.length)
+	const enumNames = enums.map(({ name }) => name)
 
-  // Per model payload interface.
-  const modelPayloadParams = modelNames.map(
-    (mname) => `export interface ${mname}PayloadParams<T> {
-    model: "${mname}"
-    Payload: Prisma.${mname}GetPayload<T>
-}`
-  )
+	const typeImports = ['Prisma', 'PrismaClient', ...enumNames, ...modelNames]
 
-  // Model map.
-  const modelMap = modelNames.map((n) => ` ${n}: ${n}`)
+	// Imports
+	module.addImportDeclarations([
+		{
+			namedImports: ['KeyFor', 'AnyModel', 'AnyArgs', 'MaybeAnyArgs'],
+			moduleSpecifier: 'prisma-model-types',
+			isTypeOnly: true,
+		},
+		{
+			namedImports: typeImports,
+			moduleSpecifier: '@prisma/client',
+			isTypeOnly: true,
+		},
+	])
 
-  // Model enum util.
-  const modelEnum = modelNames.map((n) => `  ${n} = '${n}'`)
+	// Model name
+	module.addVariableStatements([
+		{
+			declarations: [
+				{
+					name: 'ModelNames',
+					initializer: `Prisma.ModelNames`,
+				},
+			],
+			isExported: true,
+		},
+		{
+			isExported: true,
+			declarations: [
+				{
+					name: 'ModelName',
+					type: 'Prisma.ModelName',
+				},
+			],
+		},
+	])
 
-  // Schema `enum` objects.
-  const enumNames = enums.map(({ name }) => name)
-  const enumLines = enumNames.map((n) => `export const ${n}s = pkg.${n}`)
+	// ModelDelegate
+	module.addTypeAlias({
+		name: 'ModelDelegate',
+		leadingTrivia: '// Base interface matching any model delegate.',
+		isExported: true,
+		type: (writer) =>
+			writer.block(() =>
+				writer.write(`[key in Prisma.PrismaAction]?: (...args: any[]) => any`)
+			),
+	})
 
-  const typeImports = ['Prisma', 'PrismaClient', ...enumNames, ...modelNames]
+	// Model Params base.
+	module.addInterface({
+		name: 'ModelParams',
+		isExported: true,
+		properties: [
+			{
+				name: 'model',
+				type: 'ModelName',
+			},
+			...INTERFACE_NAMES.map((propName) => ({
+				name: propName,
+				type: 'any',
+			})),
+			{
+				name: 'Delegate',
+				type: 'ModelDelegate',
+			},
+		],
+	})
 
-  // language=typescript
-  const interfaceTmpl = `
-import type { ${typeImports.join(', ')} } from '@prisma/client'
-import pkg from '@prisma/client'
+	// Model parameters.
+	const paramsInterfaces = modelNames.map<
+		OptionalKind<InterfaceDeclarationStructure>
+	>((name) => {
+		const paramsProps = INTERFACE_NAMES.map<
+			OptionalKind<PropertySignatureStructure>
+		>((propName) => ({
+			name: propName,
+			type: `Prisma.${name}${propName}`,
+		}))
 
-export const ModelNames = pkg.Prisma.ModelName
-export type ModelName = Prisma.ModelName
+		return {
+			name: `${name}Params`,
+			leadingTrivia: `// ${name} model parameters.`,
+			isExported: true,
+			properties: [
+				{
+					name: 'model',
+					type: `"${name}"`,
+				},
+				...paramsProps,
+				{
+					name: 'Delegate',
+					type: `Prisma.${name}Delegate<Prisma.RejectOnNotFound | Prisma.RejectPerOperation>`,
+				},
+			],
+		}
+	})
 
-export const QueryModes = pkg.Prisma.QueryMode
-export type QueryMode = Prisma.QueryMode
+	module.addInterfaces(paramsInterfaces)
+	module.addInterface({
+		name: 'ParamsByModel',
+		isExported: true,
+		leadingTrivia: '// All model params keyed by model.',
+		properties: modelNames.map((name) => ({
+			name,
+			type: `${name}Params`,
+		})),
+	})
 
-// Base interface matching any model delegate.
-export type ModelDelegate = { [key in Prisma.PrismaAction]?: (...args: any[]) => any }
+	// payload
+	const payloadInterfaces = modelNames.map<
+		OptionalKind<InterfaceDeclarationStructure>
+	>((name) => ({
+		name: `${name}PayloadParams`,
+		leadingTrivia: `// ${name} payload parameters.`,
+		isExported: true,
+		typeParameters: [
+			{
+				name: 'T',
+				constraint: 'MaybeAnyArgs',
+			},
+		],
+		properties: [
+			{
+				name: 'model',
+				type: `"${name}"`,
+			},
+			{
+				name: 'Payload',
+				type: `${name}PayloadParams<T>`,
+			},
+		],
+	}))
+	module.addInterfaces(payloadInterfaces)
 
+	// Model enum.
+	module.addEnum({
+		name: 'Model',
+		leadingTrivia: [
+			'/** Helper enum of Model names.',
+			' * Just allows for:',
+			' * SomeType<Model.User> as an alternative too:',
+			` * SomeType<'User'> or SomeType<typeof Models.User>`,
+			' */\n',
+		],
+		isExported: true,
+		members: modelNames.map((name) => ({
+			name,
+			type: `"${name}"`,
+		})),
+	})
 
-// Base interface for model params.
-export interface ModelParams {
-    model: ModelName
-${interfaceNames.map((n) => `  ${n}: any`)}  
-    Delegate: ModelDelegate
+	// Model interfaces.
+	module.addInterface({
+		name: 'ModelInterfaces',
+		isExported: true,
+		leadingTrivia: [
+			'/** Model Map',
+			' * Useful for mapping model interfaces <-> model names (among other things.)',
+			' * @see https://stackoverflow.com/a/53431302/2666223',
+			' */\n',
+		],
+		properties: modelNames.map((name) => ({ name, type: `"${name}"` })),
+	})
+
+	// Helpers
+	module.addStatements([(writer) => writer.write(HELPERS)])
+
+	module.addExportDeclaration({
+		namedExports: typeImports,
+	})
+
+	return source
 }
 
-
-${modelParams.join('\n\n')}
-
-
-// All model params keyed by model.
-export interface ParamsByModel {
-${modelNames.map((mname) => `  ${mname}: ${mname}Params`)}
+export const generate = () => {
+	const pkgPath = fileURLToPath(new URL('..', import.meta.url))
+	// Setup prisma handler.
+	generatorHandler({
+		onManifest(config: GeneratorConfig): GeneratorManifest {
+			logger.info('Prisma Model Types Generator Loaded!')
+			logger.info(config.name)
+			return {
+				defaultOutput: pkgPath ?? './',
+				prettyName: 'prisma-model-types-generator',
+				version: '1.0.0',
+				requiresGenerators: ['prisma-client-js'],
+			}
+		},
+		async onGenerate(options: GeneratorOptions): Promise<any> {
+			const outPath = path.join(
+				options.generator.output!.value! ?? pkgPath,
+				'prisma-model-types.d.ts'
+			)
+			logger.info(`Writing to ${outPath} (pkgPath: ${pkgPath})`)
+			const source = build({
+				filePath: outPath,
+				models: options.dmmf.datamodel.models,
+				enums: options.dmmf.datamodel.enums,
+			})
+			await fse.outputFile(outPath, source.getFullText())
+		},
+	})
 }
-
-
-// Base interface for model payload params.
-export interface ModelPayloadParams {
-  model: ModelName
-  Payload: any
-}
-
-
-${modelPayloadParams.join('\n\n')}
-
-
-// All Payload params types keyed by model. 
-export interface PayloadParamsByModel<T> {
-${modelNames.map((mname) => `  ${mname}: ${mname}PayloadParams<T>`)}
-}
-
-// Helper enum of Model names. 
-// Just allows for:
-// SomeType<Model.User> instead of:
-// SomeType<'User'> or SomeType<typeof Models.User>
-export enum Model {
-${modelEnum.join(',\n')}
-}
-
-// Enums
-${enumLines.join('\n')}
-
-
-/**
- * Model Map.
- * Useful for mapping model interfaces <-> model name (among other things).
- * @see - https://stackoverflow.com/a/53431302/2666223
- */
-export interface ModelInterfaces {
-${modelMap.join('\n')}
-}
-
-
-export { ${typeImports.join(', ')} }
-
-`
-  return interfaceTmpl
-}
-
-// Setup prisma handler.
-generatorHandler({
-  onManifest(config: GeneratorConfig): GeneratorManifest {
-    logger.info('Types Gen Loaded!')
-    logger.info(config.name)
-    return {
-      defaultOutput: '../src/types',
-      prettyName: 'types-gen',
-      version: '1.0.0'
-    }
-  },
-  async onGenerate(options: GeneratorOptions): Promise<any> {
-    const outPath = path.join(options.generator.output!.value, 'types.ts')
-    const res = createInterface(
-      options.dmmf.datamodel.models,
-      options.dmmf.datamodel.enums
-    )
-    await fse.outputFile(outPath, res)
-  }
-})
--- packages/prisma-model-types/src/type-utils.ts
+++ packages/prisma-model-types/src/type-utils.ts
@@ -1,154 +1,33 @@
-import type { KeyFor } from '@prisma-model-types/shared'
-import type { PrismaPromise } from '@prisma/client'
-import pkg from '@prisma/client'
-import type * as ModelTypes from './types'
-import type {
-  ModelInterfaces,
-  ModelName,
-  ParamsByModel,
-  PayloadParamsByModel
-} from './types'
-
-export interface AnyModel {
-  id?: string
-  dateCreated?: Date | string
-  dateUpdated?: Date | string
-  dateInvalidated?: Date | string | null
-
-  [key: string]: any
+export type RemoveNeverProps<T> = {
+	[K in Exclude<
+		keyof T,
+		{
+			[P in keyof T]: T[P] extends Function ? P : never
+		}[keyof T]
+	>]: T[K]
 }
 
-/**
- * Take model name (as string) or model interface exclusively.
- */
-export type ModelNameOrModel = ModelName | AnyModel
-
-export type IsAnyModelOrModelName<T> = [ModelNameOrModel] extends [T]
-  ? 1
-  : [ModelName] extends [T]
-  ? 1
-  : 0
+export type IncludeProp<T extends object, E> = RemoveNeverProps<{
+	[K in keyof T]: T[K] extends E ? T[K] : never
+}>
 
 /**
- * Resolve (string) name from model name or model interface.
+ * Obtain key for given value from an object.
  */
-export type NameForModel<ModelT> = IsAnyModelOrModelName<ModelT> extends 1
-  ? ModelName
-  : ModelT extends ModelName
-  ? ModelT
-  : ModelT extends ModelInterfaces[keyof ModelInterfaces]
-  ? KeyFor<ModelInterfaces, ModelT>
-  : never
+export type KeyFor<
+	ObjectT extends object,
+	ValueT extends ObjectT[keyof ObjectT]
+> = keyof IncludeProp<ObjectT, ValueT>
 
-/**
- * Resolve (interface) model from model name or model.
- */
-export type ModelFromName<ModelOrName> =
-  IsAnyModelOrModelName<ModelOrName> extends 1
-    ? AnyModel
-    : ModelOrName extends ModelName
-    ? ModelInterfaces[ModelName]
-    : ModelOrName extends ModelInterfaces[keyof ModelInterfaces]
-    ? ModelInterfaces[NameForModel<ModelOrName>]
-    : never
+export interface AnyModel {
+	id?: string
 
-/**
- * Resolve Prisma args/action/input/output/etc. type for given model.
- */
-export type PrismaTypeFor<
-  ModelT,
-  Name extends keyof ModelTypes.ModelParams,
-  _Model extends string = NameForModel<ModelT>
-> = IsAnyModelOrModelName<ModelT> extends 1
-  ? ModelTypes.ModelParams[Name]
-  : _Model extends keyof ParamsByModel
-  ? ParamsByModel[_Model][Name]
-  : ModelTypes.ModelParams[Name]
+	[key: string]: any
+}
 
-export type ModelParamsFor<
-  ModelT,
-  _Model = NameForModel<ModelT>
-> = IsAnyModelOrModelName<ModelT> extends 1
-  ? ModelTypes.ModelParams
-  : _Model extends keyof ParamsByModel
-  ? ParamsByModel[_Model]
-  : ModelTypes.ModelParams
+export interface AnyArgs {
+	select?: any
+	include?: any
+}
 
-export type CreateFor<T extends ModelNameOrModel> = PrismaTypeFor<
-  T,
-  'CreateInput'
->
-
-export type UpdateFor<T extends ModelNameOrModel> = PrismaTypeFor<
-  T,
-  'UpdateInput'
->
-
-export type ArgsFor<T extends ModelNameOrModel> = PrismaTypeFor<T, 'Args'>
-export type CreateArgsFor<T extends ModelNameOrModel> = PrismaTypeFor<
-  T,
-  'CreateArgs'
->
-export type UpdateArgsFor<T extends ModelNameOrModel> = PrismaTypeFor<
-  T,
-  'UpdateArgs'
->
-
-export type UpsertArgsFor<T extends ModelNameOrModel> = PrismaTypeFor<
-  T,
-  'UpsertArgs'
->
-
-export type FindManyArgsFor<T extends ModelNameOrModel> = PrismaTypeFor<
-  T,
-  'FindManyArgs'
->
-
-export type FindFirstArgsFor<T extends ModelNameOrModel> = PrismaTypeFor<
-  T,
-  'FindFirstArgs'
->
-
-export type FindUniqueArgsFor<T extends ModelNameOrModel> = PrismaTypeFor<
-  T,
-  'FindUniqueArgs'
->
-
-export type SelectFor<T extends ModelNameOrModel> = PrismaTypeFor<T, 'Select'>
-
-export type IncludeFor<T extends ModelNameOrModel> = PrismaTypeFor<T, 'Include'>
-
-export type WhereFor<T extends ModelNameOrModel> = PrismaTypeFor<
-  T,
-  'WhereInput'
->
-
-export type WhereUniqueFor<T extends ModelNameOrModel> = PrismaTypeFor<
-  T,
-  'WhereUniqueInput'
->
-
-export type DelegateFor<T extends ModelNameOrModel> = PrismaTypeFor<
-  T,
-  'Delegate'
->
-
-export const validatorFor = <
-  ModelT extends ModelNameOrModel,
-  _ModelArgs = ArgsFor<ModelT>
->() => pkg.Prisma.validator<_ModelArgs>()
-
-/**
- * Resolve Prisma args/action/input/output/etc. type for given model.
- */
-export type PayloadFor<
-  ModelT extends ModelNameOrModel,
-  T extends ArgsFor<ModelT> | any = undefined,
-  _Model extends string = NameForModel<ModelT>
-> = IsAnyModelOrModelName<ModelT> extends 1
-  ? ModelTypes.ModelPayloadParams['Payload']
-  : _Model extends keyof PayloadParamsByModel<any>
-  ? PayloadParamsByModel<T>[_Model]['Payload']
-  : ModelTypes.ModelPayloadParams['Payload']
-
-export type { PrismaPromise }
+export type MaybeAnyArgs = boolean | AnyArgs | null | undefined
